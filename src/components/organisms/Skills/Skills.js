d132df48fdfcaff52724e17b3c57f7179d974678
Revku <revkuu@gmail.com>
2022-09-10 17:08:45 +0200

    Add title of skills section

---
+++ src/components/organisms/Skills/Skills.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as styles from './Skills.module.scss'
+
+import Section from '@templates/SectionTemplate/SectionTemplate'
+
+const Skills = () => {
+  return (
+      <Section title={"Technologies & Skills"}>
+
+      </Section>
+  )
+}
+
+export default Skills
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import { SEO } from '@components/SEO'
 import PageTemplate from '@templates/PageTemplate/PageTemplate'
 import Header from '@organisms/Header/Header'
 import About from '@organisms/About/About'
+import Skills from '@organisms/Skills/Skills'
 
 const IndexPage = () => {
   return (
     <PageTemplate>
         <Header />
         <About />
+        <Skills />
     </PageTemplate>
   )
 }
