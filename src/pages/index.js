3331d59bd3c65af0114ab6aafbf83611d774727f
Revku <revkuu@gmail.com>
2022-09-10 15:01:10 +0200

    Add about me section with basic lorem ipsum

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { SEO } from '@components/SEO'
+
 import PageTemplate from '@templates/PageTemplate/PageTemplate'
 import Header from '@organisms/Header/Header'
+import About from '@organisms/About/About'
 
 const IndexPage = () => {
   return (
     <PageTemplate>
-      <Header />
+        <Header />
+        <About />
     </PageTemplate>
   )
 }
