cc153b6a4784b72295a04fa80c46f50fccdc3a28
Revku <office@revku.dev>
2022-11-01 16:31:57 +0100

    Update about section

--- src/components/organisms/About/About.js
+++ src/components/organisms/About/About.js
@@ -2,22 +2,29 @@ import React from 'react';
 import * as styles from './About.module.scss';
 
 import Section from 'components/templates/SectionTemplate/SectionTemplate';
+import aboutImage from 'images/about.svg';
 
 const About = () => {
   return (
     <Section title={'O mnie'}>
-      <p className={styles.paragraph}>
-        <b>Cześć 👋, nazywam się Patryk.</b>
-        <br /> <b>Revku</b> to mój pseudonim i jednocześnie marka, którą tworzę
-        i staram cały czas rozwijać. Tworzę aplikacje do sieci, zajmuję się
-        głównie <b>front-end'em</b> czyli w ogromnym skrócie warstwą wizualną i
-        interaktywną stron internetowych. Programowaniem interesuję się od 12
-        roku życia, a tworzenia stron internetowych zacząłem uczyć się około 2
-        lata temu. W przyszłości chciałbym nauczyć się tworzenia aplikacji
-        mobilnych. Aktualnie jestem uczniem technikum w kierunku “programista”,
-        cały czas staram się rozwijać swoje umiejętności techniczne, ale także
-        miękkie.
-      </p>
+      <div className={styles.wrapper}>
+        <p className={styles.paragraph}>
+          Cześć! Nazywam się Patryk. <br />
+          <br />
+          Tworzę aplikacje do sieci, zajmuję się głównie <b>
+            front-end'em
+          </b>{' '}
+          czyli w ogromnym skrócie warstwą wizualną i interaktywną stron
+          internetowych. Programowaniem interesuję się od 12 roku życia, a
+          tworzenia stron internetowych zacząłem uczyć się około 2 lata temu.{' '}
+          <br />
+          <br />W przyszłości chciałbym nauczyć się tworzenia aplikacji
+          mobilnych. Aktualnie jestem uczniem technikum w kierunku
+          “programista”, cały czas staram się rozwijać swoje umiejętności
+          techniczne, ale także miękkie.
+        </p>
+        <img className={styles.image} src={aboutImage} alt="About me image" />
+      </div>
     </Section>
   );
 };
